6a73933f3b813c83e3d55d34d2c05b96c205b111
MarriNagaLakshmi <Batch1@VITUNIVERSITY.LOCAL>
2026-08-06 10:34:24 +0530

    Add circle_area function

--- area.py
+++ area.py
@@ -1,6 +1,10 @@
 def rectangle_area(length, breadth):
     """Calculate and return the area of a rectangle."""
     return length * breadth
+def circle_area(radius):
+    """Calculate and return the area of a circle."""
+    return 3.14 * radius * radius
+
 
 if __name__ == '__main__':
     # Test cases
